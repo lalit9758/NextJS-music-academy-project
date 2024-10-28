a5e8ef7e74e23302bf04742d88cbe5a2bf47e3ca
lalit kumar <143026179+lalit9758@users.noreply.github.com>
2024-10-28 20:00:21 +0530

    hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroSelction from "@/components/HeroSelction";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <h1 className="text-2xl text-center">Chai aur code</h1>
+      <HeroSelction/>
     </main>
   );
 }
---
+++ src/components/HeroSelction.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function HeroSelction() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center mx-auto py-10 md:py-0">
+      HeroSelction
+    </div>
+  );
+}
+
+export default HeroSelction;
